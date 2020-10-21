f52ebc9c71e8aa7f1787d966b3b1a4c3ad504c6d
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-10-21 20:29:31 -0300

    add output calculations and tests

--- flaskr/covid19.py
+++ flaskr/covid19.py
@@ -1,6 +1,7 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, flash, g, redirect, render_template, request, url_for
 )
+import requests
 
 from werkzeug.exceptions import abort
 
@@ -9,3 +10,39 @@ bp = Blueprint('covid19', __name__)
 @bp.route('/')
 def index():
     return render_template('covid19/index.html')
+
+
+@bp.route('/show', methods=('GET', 'POST'))
+def show():
+    days = request.form['days']
+    error = None
+
+    if not days:
+        error = 'Days is required.'
+
+    if not days.isnumeric():
+        error = 'Days must be a number.'
+
+    if error is not None:
+        flash(error)
+    else:
+        result = requests.get('https://thevirustracker.com/free-api?global=stats')
+        data = result.json()
+
+        current_cases = data['results'][0]['total_new_cases_today']
+        transmission_factor = 1.03
+        balancer = 2551.54
+
+        cases = []
+        count = 0
+        while count < int(days):                
+            x = current_cases
+            res = transmission_factor * x + balancer
+
+            cases.append(res)
+            current_cases = res
+            count += 1
+        
+        return render_template('covid19/show.html', cases=cases)
+            
+    return redirect(url_for('covid19.index'))
--- tests/test_covid19.py
+++ tests/test_covid19.py
@@ -4,3 +4,12 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"Days" in response.data
+
+def test_post_show(client):
+    response = client.post("/show", data={"days": "2"})
+    assert response.status_code == 200
+    assert b"Cases" in response.data
+
+def test_post_show_integer_only(client):
+    response = client.post("/show", data={"days": "xx"})
+    assert response.status_code == 302
